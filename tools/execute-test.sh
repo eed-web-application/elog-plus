7387948d32603fed8f938ed7515e7906191c1cea
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-06-20 14:41:31 -0700

    fixes

--- tools/execute-test.sh
+++ tools/execute-test.sh
@@ -1,4 +1,21 @@
-#!/usr/bin/env bash
-echo "Starting test"
-/opt/wait;
-/opt/app/gradlew -b /opt/app/build.gradle test
+#!/bin/bash
+file_array=()
+# Find all files with names starting with 'docker-compose.'
+files=$(find $PWD -name 'docker-compose.*')
+
+# Iterate over the files
+for file in $files; do
+  echo "Add $file"
+   file_array+=" -f $file"
+  # Your processing code here...
+done
+
+# Check if the array is empty
+if [ -z "${file_array[*]}" ]; then
+  echo "No files found"
+else
+  # Call your command with all the file paths as arguments
+  ./your-command "${file_array[@]}"
+fi
+
+docker compose "${file_array[@]}" up --attach test  --exit-code-from test
