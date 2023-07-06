0169ddc12f77844e2de66f8bf3e49e5424a4cb15
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-07-05 17:34:12 -0700

    add mongock for data migration management on mongodb

--- src/main/java/edu/stanford/slac/elog_plus/App.java
+++ src/main/java/edu/stanford/slac/elog_plus/App.java
@@ -1,8 +1,10 @@
 package edu.stanford.slac.elog_plus;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableMongock
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
---
+++ src/main/java/edu/stanford/slac/elog_plus/migration/InitIndex.java
@@ -0,0 +1,31 @@
+package edu.stanford.slac.elog_plus.migration;
+
+
+import edu.stanford.slac.elog_plus.model.Attachment;
+import edu.stanford.slac.elog_plus.model.Log;
+import edu.stanford.slac.elog_plus.model.Tag;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@AllArgsConstructor
+@ChangeUnit(id = "database-initializer", order = "1", author = "bisegni")
+public class InitIndex {
+    private final MongoTemplate mongoTemplate;
+    private final MongoMappingContext mongoMappingContext;
+
+    @Execution
+    public void changeSet() {
+        MongoDDLOps.createIndex(Log.class, mongoTemplate, mongoMappingContext);
+        MongoDDLOps.createIndex(Attachment.class, mongoTemplate, mongoMappingContext);
+        MongoDDLOps.createIndex(Tag.class, mongoTemplate, mongoMappingContext);
+    }
+
+    @RollbackExecution
+    public void rollback() {
+
+    }
+}
