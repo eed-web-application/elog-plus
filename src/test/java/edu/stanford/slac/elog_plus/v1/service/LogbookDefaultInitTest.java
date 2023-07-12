131c5f008dae0d0d28a2d651050fce9460b7d30e
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-07-12 15:41:09 -0700

    fixes

--- src/test/java/edu/stanford/slac/elog_plus/v1/service/LogbookDefaultInitTest.java
+++ src/test/java/edu/stanford/slac/elog_plus/v1/service/LogbookDefaultInitTest.java
@@ -2,7 +2,9 @@ package edu.stanford.slac.elog_plus.v1.service;
 
 import edu.stanford.slac.elog_plus.api.v1.dto.LogbookDTO;
 import edu.stanford.slac.elog_plus.migration.InitLogbook;
+import edu.stanford.slac.elog_plus.model.Logbook;
 import edu.stanford.slac.elog_plus.service.LogbookService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -20,12 +26,24 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 @SpringBootTest()
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles(profiles = "test")
 public class LogbookDefaultInitTest {
     @Autowired
     LogbookService logbookService;
+    @Autowired
+    MongoTemplate mongoTemplate;
+    @Autowired
+    MongoMappingContext mongoMappingContext;
+
+    @BeforeEach
+    public void preTest() {
+        mongoTemplate.remove(new Query(), Logbook.class);
+    }
 
     @Test
     public void checkDefaultLogbookCreation() {
+        InitLogbook initLogbook = new InitLogbook(logbookService, mongoTemplate, mongoMappingContext);
+        initLogbook.changeSet();
         List<LogbookDTO> allDefault = assertDoesNotThrow(
                 () -> logbookService.getAllLogbook()
         );
--- src/test/java/edu/stanford/slac/elog_plus/v1/service/LogbookServiceTest.java
+++ src/test/java/edu/stanford/slac/elog_plus/v1/service/LogbookServiceTest.java
@@ -2,7 +2,6 @@ package edu.stanford.slac.elog_plus.v1.service;
 
 import edu.stanford.slac.elog_plus.api.v1.dto.LogbookDTO;
 import edu.stanford.slac.elog_plus.api.v1.dto.NewLogbookDTO;
-import edu.stanford.slac.elog_plus.model.Log;
 import edu.stanford.slac.elog_plus.model.Logbook;
 import edu.stanford.slac.elog_plus.service.LogbookService;
 import org.junit.jupiter.api.BeforeEach;
