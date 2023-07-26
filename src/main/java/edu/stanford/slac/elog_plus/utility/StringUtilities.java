de24a8e94d32218f3e939d63ccdce7aa0b40c0a9
Claudio Bisegni <bisegni@users.noreply.github.com>
2023-07-26 10:28:26 -0700

    fix html sanitifiation adding header tag to safe list (#75)
    - fix html sanitifiation adding header tag to safe list
    +semrel: patch

--- src/main/java/edu/stanford/slac/elog_plus/utility/StringUtilities.java
+++ src/main/java/edu/stanford/slac/elog_plus/utility/StringUtilities.java
@@ -1,5 +1,8 @@
 package edu.stanford.slac.elog_plus.utility;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
 import java.text.Normalizer;
 
 public class StringUtilities {
@@ -19,5 +22,15 @@ public class StringUtilities {
                         Normalizer.Form.NFD
                 ).replaceAll("[^\\p{ASCII}]", "")
                 .replaceAll(" ", "-");
+    }
+
+    public static String sanitizeEntryTitle(String title) {
+        return Jsoup.clean(title, Safelist.basic());
+    }
+
+    public static String sanitizeEntryText(String text) {
+        Safelist safelist = Safelist.basic();
+        safelist.addTags("h1", "h2", "h3", "h4");
+        return Jsoup.clean(text, safelist);
     }
 }
