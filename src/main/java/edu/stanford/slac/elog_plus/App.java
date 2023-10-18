6a31ca200352b65e14daf3c1a75ec62d3d490e35
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-10-17 17:53:22 -0700

    add kafka metrics

--- src/main/java/edu/stanford/slac/elog_plus/App.java
+++ src/main/java/edu/stanford/slac/elog_plus/App.java
@@ -3,9 +3,10 @@ package edu.stanford.slac.elog_plus;
 import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 @EnableMongock
-@SpringBootApplication
+@SpringBootApplication()
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
