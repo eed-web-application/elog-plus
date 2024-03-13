3ad4374aba2207b0a1a35b3169c9ee7c90e12574
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-03-12 18:11:31 -0700

    fix date serializaiton

--- src/main/java/edu/stanford/slac/elog_plus/config/JacksonConfig.java
+++ src/main/java/edu/stanford/slac/elog_plus/config/JacksonConfig.java
@@ -2,16 +2,17 @@ package edu.stanford.slac.elog_plus.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        // Register the JavaTimeModule to handle Java 8 date/time types
-        mapper.registerModule(new JavaTimeModule());
-        return mapper;
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.modules(new JavaTimeModule());
+            // any additional customization
+        };
     }
 }
