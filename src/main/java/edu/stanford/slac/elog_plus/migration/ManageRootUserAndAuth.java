065f633010b7e4281e6fb3edb620e1f24dc21e85
Claudio Bisegni <bisegni@users.noreply.github.com>
2024-04-16 13:25:38 -0700

    add root auth management at startup (#192)
    - enable root auth management at startup

---
+++ src/main/java/edu/stanford/slac/elog_plus/migration/ManageRootUserAndAuth.java
@@ -0,0 +1,32 @@
+package edu.stanford.slac.elog_plus.migration;
+
+import edu.stanford.slac.ad.eed.base_mongodb_lib.utility.RootAuthorizationManagement;
+import edu.stanford.slac.ad.eed.baselib.service.AuthService;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.AllArgsConstructor;
+
+/**
+ * This will always run at application startup
+ * updating the root user and authorization
+ *
+ */
+@AllArgsConstructor
+@ChangeUnit(id = "manage-root-user-token-auth", order = "10000", author = "bisegni", runAlways = true)
+public class ManageRootUserAndAuth {
+    private final AuthService authService;
+    @Execution
+    public void changeSet() {
+        RootAuthorizationManagement
+                .builder()
+                .authService(authService)
+                .build()
+                .updateRootAuthorization();
+    }
+
+    @RollbackExecution
+    public void rollback() {
+
+    }
+}
