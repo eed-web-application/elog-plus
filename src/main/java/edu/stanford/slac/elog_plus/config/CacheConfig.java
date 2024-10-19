c415b3e875d6c2a2795e9cd4bbdd009a0d7e64a2
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-10-19 13:56:57 -0700

    fix for test

--- src/main/java/edu/stanford/slac/elog_plus/config/CacheConfig.java
+++ src/main/java/edu/stanford/slac/elog_plus/config/CacheConfig.java
@@ -8,6 +8,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @EnableCaching
 @Configuration
