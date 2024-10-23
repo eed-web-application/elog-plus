e5caf6865cd2a226cdc8ec48971bb465ca0e9998
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-10-23 14:28:40 -0700

    start adding descirption

--- src/main/java/edu/stanford/slac/elog_plus/model/Tag.java
+++ src/main/java/edu/stanford/slac/elog_plus/model/Tag.java
@@ -1,6 +1,7 @@
 package edu.stanford.slac.elog_plus.model;
 
 import lombok.*;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,5 +15,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Tag {
     @Id
     private String id;
+    /**
+     * The name of the tag
+     */
     private String name;
+    /**
+     * The description of the tag
+     * will be used during the selection of the right tag from the AI tools
+     */
+    private String description;
 }
